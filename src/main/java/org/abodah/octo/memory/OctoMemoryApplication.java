a06a4491f8abf8fa2d42f1c64632cac2b7844c0d
Steve Ruben <stevetuenkam@gmail.com>
2022-07-01 23:32:53 +0200

    setup base o add spring dependencies

---
+++ src/main/java/org/abodah/octo/memory/OctoMemoryApplication.java
@@ -0,0 +1,13 @@
+package org.abodah.octo.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoMemoryApplication.class, args);
+	}
+
+}
